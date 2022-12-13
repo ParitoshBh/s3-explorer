c57a6fcf2d8efe5729bc261758b792e307108637
ParitoshBh <mail@paritoshbh.me>
2022-12-12 19:07:30 -0500

    Update path for views directory

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,13 @@ import { initializeS3 } from './utils/s3';
 import { bucketGet } from "./controllers/bucket";
 import { objectGet } from "./controllers/object";
 import { parseArgs, getArgs } from "./utils/args";
+import path from 'path';
 
 const app = express();
 
 app.use(express.static('assets'))
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/../views');
+app.set('views', path.resolve(__dirname, '..', 'views'));
 
 // Parse arguments
 parseArgs();
